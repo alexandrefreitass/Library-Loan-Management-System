a70927fd31385b4b65c9e211cb32db498c3376ea
alexandrefreitass <xandy4321@hotmail.com>
2024-06-05 00:07:43 -0300

    projeto finalizado

---
+++ EmprestimoLivros/Controllers/SobreNosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmprestimoLivros.Controllers
+{
+    public class SobreNosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
