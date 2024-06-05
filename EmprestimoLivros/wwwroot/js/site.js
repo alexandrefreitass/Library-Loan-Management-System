41ce44ce6d65818bf8d7ef59e80c55b2a4ce694c
alexandrefreitass <xandy4321@hotmail.com>
2024-06-05 11:33:34 -0300

    correção jquery e js commit

--- EmprestimoLivros/wwwroot/js/site.js
+++ EmprestimoLivros/wwwroot/js/site.js
@@ -1,36 +1,38 @@
 ﻿$(document).ready(function () {
+    // Inicializa DataTable com configurações específicas
     $('#Emprestimos').DataTable({
         language: {
-            "decimal": "",
-            "emptyTable": "Nenhum registro na tabela",
-            "info": "Mostrar _START_ de _END_ de _TOTAL_ entradas",
-            "infoEmpty": "",  // Mensagem removida para quando a tabela está vazia
-            "infoFiltered": "(filtradas de um total de _MAX_ entradas)",
-            "infoPostFix": "",
-            "thousands": ",",
-            "lengthMenu": "6",  // Opção para fixar a quantidade de linhas exibidas sem permitir alteração pelo usuário
-            "loadingRecords": "Carregando...",
-            "processing": "Processando...",
-            "search": "Pesquisar:",
-            "zeroRecords": "Nenhum registro correspondente encontrado",
-            "paginate": {
-                "first": "Primeiro",
-                "last": "Último",
-                "next": "Próximo",
-                "previous": "Anterior"
+            decimal: "",
+            emptyTable: "Nenhum registro na tabela",
+            info: "Mostrar _START_ de _END_ de _TOTAL_ entradas",
+            infoEmpty: "",  // Mensagem para quando a tabela está vazia
+            infoFiltered: "(filtradas de um total de _MAX_ entradas)",
+            infoPostFix: "",
+            thousands: ",",
+            lengthMenu: "6",  // Fixa a quantidade de linhas exibidas
+            loadingRecords: "Carregando...",
+            processing: "Processando...",
+            search: "Pesquisar:",
+            zeroRecords: "Nenhum registro correspondente encontrado",
+            paginate: {
+                first: "Primeiro",
+                last: "Último",
+                next: "Próximo",
+                previous: "Anterior"
             },
-            "aria": {
-                "orderable": "Ordenar por esta coluna",
-                "orderableReverse": "Inverter a ordem por esta coluna"
+            aria: {
+                orderable: "Ordenar por esta coluna",
+                orderableReverse: "Inverter a ordem por esta coluna"
             }
         },
-        pageLength: 6,  // Mantém a definição de 6 linhas por página
-        lengthChange: false  // Desabilita a capacidade do usuário de mudar o número de itens por página
+        pageLength: 6,
+        lengthChange: false
     });
 
+    // Função para fechar alertas automaticamente após 5 segundos
     setTimeout(function () {
         $(".alert").fadeOut("slow", function () {
             $(this).alert('close');
-        })
+        });
     }, 5000);
 });
